e300dd09d6c041049e9d14847969a0451687fd54
Andrey <skamsk@list.ru>
2022-01-14 19:24:05 +0500

    add tags template

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.text import slugify
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название населенного пункта', unique=True )
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class  Meta:
+        verbose_name = 'Название населенного пункта'
+        verbose_name_plural = 'Название населенных пунктов'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(str(self.name), allow_unicode=True)
+        super().save(*args, **kwargs)
+
+class Language(models.Model): #TODO: Переименовать в тип имущества
+    name = models.CharField(max_length=50, verbose_name='Язык программирования', unique=True )
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class  Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(str(self.name), allow_unicode=False)
+        super().save(*args, **kwargs)
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    language = models.ForeignKey('language', on_delete=models.CASCADE, verbose_name='Язык программирования')
+    timestamp = models.DateField(auto_now_add=True)
+
+    class  Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+
+    def __str__(self):
+        return self.title
