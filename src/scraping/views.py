2950c132709a37170e62add4d0a524ae86bd83ea
Andrey <skamsk@list.ru>
2022-01-17 13:37:18 +0500

    add filter and form

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -3,5 +3,16 @@ from scraping.models import Vacancy
 
 
 def home_view(request):
-    qs = Vacancy.objects.all
+    # print(request.GET)
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    print(city, language)
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__name'] = city
+        if language:
+            _filter['language__name'] = language
+        qs = Vacancy.objects.filter(**_filter)
     return render(request, 'scraping/home.html', {'object_list': qs})
